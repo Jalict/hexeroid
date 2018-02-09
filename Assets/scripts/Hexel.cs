650089bb0140f368218b6a86ef60774d11d92ca5
Frans Peter Larsen <larsen.frans@gmail.com>
2018-02-09 22:50:16 +0100

    ELEMENTS

--- Assets/scripts/Hexel.cs
+++ Assets/scripts/Hexel.cs
@@ -1,19 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hexel : MonoBehaviour
 {
+    [Header("State")]
     public bool used;
     public Hexel[] neighbours;
 
+    public HexelData data;
+
+    [Header("Object References")]
+    public Text textName;
+    public SpriteRenderer hexelRenderer;
+
+    [Header("Asset References")]
+    public Sprite emptyHexelSprite;
+    public Sprite filledHexelSprite;
+
     void Start()
     {
-        
+        textName.text = data.type.ToString();
     }
 
     void Update()
     {
-        
+        // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+    }
+
+    [ContextMenu("Use")]
+    public void Use()
+    {
+        used = !used;
+
+        textName.color = !used ? Color.white : Color.black;
+
+        if (used) hexelRenderer.sprite = filledHexelSprite;
+        else hexelRenderer.sprite = emptyHexelSprite;
+    }
+
+    public void Birth()
+    {
+
+    }
+
+    public void Death()
+    {
+
+    }
+
+    public void Mutate()
+    {
+
     }
 }
---
+++ Assets/scripts/HexelData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Hexel Type", menuName = "Hexeroid/Hexels")]
+public class HexelData : ScriptableObject {
+    public HexelType type;
+
+    // time to new child
+    // needed for new child
+    // amount to generate
+}
+
+public enum HexelType
+{
+    IRIBU,
+    ALUTE,
+    CELER
+}
