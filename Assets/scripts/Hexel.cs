00c4a369693a5c11984c28b8efacc92dd0072826
Frans Peter Larsen <larsen.frans@gmail.com>
2018-02-09 21:44:00 +0100

    UNINIT

---
+++ Assets/scripts/Hexel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexel : MonoBehaviour
+{
+    public bool used;
+    public Hexel[] neighbours;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
